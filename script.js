7aac094df0456437f374f7ac3f4119824a516959
KASIMALLA-DEVARAJ <devarajkasimalla23273@gmail.com>
2025-09-30 11:30:24 +0530

    :hovers are implemented

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ search.addEventListener("click",() =>{
             const div = document.createElement("div");
             div.id = "card" + (i + 1);
             div.innerHTML = `
-                    <img src="${book.volumeInfo.imageLinks?.thumbnail}" alt="Book Cover" style="width:160px; height:150px;">
+                    <img src="${book.volumeInfo.imageLinks?.thumbnail}" alt="Book Cover" style="width:200px height:180px">
                     <h4>${book.volumeInfo.title}</h4>  
                 `;
             const container = document.getElementById("card-container");
